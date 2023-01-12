1399e66de97c6fca112bd38072a1f38b1036fa02
ShengyuanWang <78770936+ShengyuanWang@users.noreply.github.com>
2023-01-11 21:16:22 -0600

    test

--- 445f22_Shengyuan/1-sequentialLoop/getCommandLine.h
+++
@@ -1,6 +0,0 @@
-void getArguments(int argc, char *argv[],
-                  int * N, int * useConstantSeed);
-
-int isNumber(char s[]);
-void exitWithError(char cmdFlag, char ** argv);
-void Usage(char *program);
